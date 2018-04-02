603afdf9c6495b3da0dea46f5dfad9d55faa71ef
Jamila <J.cornick21@gmail.com>
2018-04-01 22:02:02 -0700

    making sure all files are up to date

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,42 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  a = 0
+  b = my_words.length
+
+  while a < b
+
+    while my_words[a] == " "
+      a += 1
+    end
+
+    first_word = a
+
+    while my_words[first_word] != " " && first_word < my_words.length - 1
+      first_word += 1
+    end
+
+    c = a
+    d = first_word
+
+
+    while c < d
+      while my_words[c] == " "
+        c += 1
+      end
+
+      while my_words[d] == " "
+        d -= 1
+      end
+      temp_value = my_words[c]
+      my_words[c] = my_words[d]
+      my_words[d] = temp_value
+
+      c += 1
+      d -= 1
+    end
+
+    a = first_word + 1
+  end
+  return my_words
+  # raise NotImplementedError
 end
